9635e706034a21c422d5b20f37268e8ade98f5e6
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-09-27 21:17:54 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,13 @@
+#pattern
+#G.Niharika
+n=int(input("rows="))
+for i in range(0,n):
+    print((n-i) , end=" ")
+    k=0.1
+    for j in range(0,i):
+        print("{:.1f}".format(k),end=" ")
+        k=k+0.1
+    print("\r")
+       
+       
+
