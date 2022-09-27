46d0ec155d01561c6e12db8a42f83fb23349f9d0
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-09-27 21:06:57 +0530

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,8 @@
+#program to find a number is even or odd
+#G.Niharika
+print("enter the number")
+n=int(input("n="))
+if(n%2==0):
+    print("the number is a even number")
+else:
+    print("the number is odd number")
