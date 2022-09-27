9c3ca7094143410451c3e46b6850d7824971fd74
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-09-27 21:12:50 +0530

    Add files via upload

---
+++ simple interst.py
@@ -0,0 +1,8 @@
+#program to calculate simple interst for the given principal amount
+#G.Niahrika
+print("enter the values")
+p=int(input("principal amount="))
+t=int(input("time="))
+r=int(input("rate="))
+simple_interst=(p*t*r)/100
+print("simple interst=",simple_interst)
