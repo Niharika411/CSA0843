db8e4c90ec3ce04118a369fb6b7ecb18cb326172
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-09-27 21:09:07 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,26 @@
+#program to calculate values
+#G.Niharika
+print("enter the values")
+x=int(input("x="))
+n=int(input("n="))
+print(" 1 for power\n 2 for addition\n 3 for subtraction\n 4 for multiplication\n 5 for division\n")
+choice=int(input("enter your choice"))
+pow=x**n
+add=x+n
+sub=x-n
+mul=x*n
+div=x/n
+if(choice==1):
+    print("power of x and n is:",pow)
+elif(choice==2):
+    print("addition of x and n is:",add)
+elif(choice==3):
+    print("subtraction of x and n is:",sub)
+elif(choice==4):
+    print("multiplacation of x and n is:",mul)
+elif(choice==5):
+    print("division of x and n is:",div)
+else:
+    print("invalid input")
+
+    
