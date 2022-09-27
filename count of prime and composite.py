cc05b995f1f06e1c97901f3f6addb6b5a85041b1
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-09-27 21:02:49 +0530

    Add files via upload

---
+++ count of prime and composite.py
@@ -0,0 +1,30 @@
+#program to count number of prime and composite numbers
+#G.Niharika
+mylist=[]
+try:
+    n=int(input("enter length of list:"))
+    for a in range(1,n+1):
+        e=int(input("enter the element:"))
+        mylist.append(e)
+    print(mylist)
+    i=len(mylist)
+    prime=[]
+    composite=[]
+    for i in range(0,i):
+        c=0
+        for j in range(2,mylist[i]):
+            if (mylist[i]%j==0):
+                c=1
+                break
+        if (c==0):
+            prime.append(mylist[i])
+        else:
+            composite.append(mylist[i])
+    print("prime number list is ",prime)
+    print("composite number list is ",composite)
+    x=len(prime)
+    print("Number of Prime Numbers are:",x)
+    y=len(composite)
+    print("Number of Composite Numbers are:",y)
+except ValueError:
+    print("enter integer number")
