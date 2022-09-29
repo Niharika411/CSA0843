d2057b692e3d8055c94df96dd9e8939a0ff91687
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-09-29 11:18:43 +0530

    Add files via upload

---
+++ Rev_Num.py
@@ -0,0 +1,12 @@
+try:
+    n = int(input("Enter the integer number: "))
+    revs_number = 0
+    if (n > 0):
+        remainder = n % 10
+        revs_number = (revs_number * 10) + remainder
+        n = n // 10
+        print("The reverse number is : {}".format(revs_number))
+    else:
+        print("Enter Valid Input")
+except ValueError:
+    print("INVALID INPUT")
