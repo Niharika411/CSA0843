80ebc8d11c720aff8358786c1debdf87c3d96d2d
Niharika1411 <112242874+Niharika411@users.noreply.github.com>
2022-10-31 11:45:18 +0530

    Update Common Factors.py

--- Common Factors.py
+++ Common Factors.py
@@ -1,10 +1,12 @@
-n=input("enter the character to be printerd=")
-r=int(input("Max number of time to be printed="))
-if(r<=0):
-	print("INVALID INPUT")
-	exit()
-else:
-	for i in range(0,r):
-		for j in range(0,i+1):
-			print(n,end=" ")
-		print()
+n=int(input("Enter the number of elements="))
+arr=[]
+for i in range(0,n):
+    a=int(input("enter the element="))
+    arr.append(a)
+print(arr)
+count=0
+for j in range(1,arr[i]+1):
+    if(arr[i]%j==0):
+        count+=1
+print("FACTORS= ",count-2)
+
